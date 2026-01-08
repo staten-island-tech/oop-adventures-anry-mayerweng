a4842364de32238ee6484dea2f01923640c9ccc8
Andy Weng <andyweng257@gmail.com>
2026-01-08 10:16:45 -0500

    Update crafting.py

--- crafting.py
+++ crafting.py
@@ -1,5 +1,5 @@
 
-player_invetory =["Iron","Iron", "Bone", "Vampire Essence"]
+player_invetory =["Vampire Essence", "Vampire Essence", "Vampire Essence", "Vampire Essence", "Vampire Essence"]
 crafting_recipes = [
     
         {"item":"Iron Sword",
@@ -24,15 +24,16 @@ class craftingfunc():
         {"item":"Iron Sword",
           "recipe":"Iron "*2},
         {"item":"Blood Wand", 
-         "recipe": "Vampire Essense "*5},
+         "recipe":"Vampire Essence " * 5},
         {"item":"Blood Sword", 
-         "recipe":"Vampire Essense, Vampire Essense, Vampire Essense, Vampire Essense, Vampire Essense, Iron, Iron"},
+         "recipe":"Vampire Essence, Vampire Essence, Vampire Essence, Vampire Essence, Vampire Essence, Iron, Iron"},
         {"item":"Vampire Mask",
          "recipe":"Vampire Essence, Vampire Essence, Bones, Bones, Bones"}
         ]
 
 
     def inputmats(self):
+        global playerinvecopy
         playerinvecopy = player_invetory.copy()
         while True:
             iteminput = input("Add items to craft: ").title()
@@ -47,22 +48,34 @@ class craftingfunc():
 
 
     def checkcraft(self, desired_item):
-        for recipes in self.recipes:
-            if recipes["item"].title() == desired_item.title():
-                self.chosen_item_index = recipes["item"].index(recipes["item"])
+        for recipes, dic in enumerate(self.recipes):
+            if dic["item"] == desired_item:
+                self.chosen_item_index = recipes
+            #if recipes["item"].title() == desired_item.title():
+                #self.chosen_item_index = self.recipes.index(recipes["item"])
 
-            if self.chosen_item_index == None:
-                return
-            
-        print(self.chosen_item_index)
+        if self.chosen_item_index == None:
+            print("Not Real item bro")
+        
 
 
     def craft_item(self):
-        if self.item_used.sort() == self.recipes[self.chosen_item_index]["recipe"]:
-            print("work")
-        print(self.item_used)
-        print(self.recipes[self.chosen_item_index]["recipe"].split().sort())
+        can_give_item = False
+        if self.item_used.strip() == self.recipes[self.chosen_item_index]["recipe"].split().strip():
+            for item in self.item_used:
+                if item in player_invetory:
+                    player_invetory.remove(item)
+                    can_give_item = True
+        else:
+            print("you cant  craft it no valid mats")
+        if can_give_item == True:
+            player_invetory.append(self.recipes[self.chosen_item_index]["item"])
         
+        
+        playerinvecopy.clear()
+        self.item_used.clear()
+        print(player_invetory)
+        print(self.recipes[self.chosen_item_index]["recipe"].split())
                
 
 crafting = craftingfunc()
