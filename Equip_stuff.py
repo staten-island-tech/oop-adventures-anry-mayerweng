50f4bc549c1b47f1f72078e244d15bfbbec9b042
Andy Weng <andyweng257@gmail.com>
2026-01-12 10:14:26 -0500

    moved stuff

--- Equip_stuff.py
+++ Equip_stuff.py
@@ -1,9 +1,52 @@
 import playerClasses as pf
 
+stats_armors = [
+    {"item":"Vampire Mask",
+    "statsHP":70,
+    "statsDEF":5},
+     {"item":"Iron Chestplate",
+    "statsHP":30,
+    "statsDEF":30},
+     {"item":"Iron Leggings",
+    "statsHP":15,
+    "statsDEF":30},
+     {"item":"Iron Boots",
+    "statsHP":10,
+    "statsDEF":20},
+]
+
+stats_Gears_and_stuff = [
+    {"item":"Blood Sword",
+     "statsATK":20,
+     "statsSPD":2},
+     {"item":"Blood Wand",
+     "statsATK":10,
+     "statsSPD":1},
+     {"item":"Blood Sword",
+     "statsATK":12,
+     "statsSPD":5},
+]
+
+
+
+
+
+
+
 
 chosenClass = pf.chosenClass
 chosenData = pf.chosenData
 player = pf.player
 
-print(player.HP)
+
+class Armor:
+    def __init__(self, hp, defense):
+        self.hp = hp
+        self.defense = defense
+
+
+class Gear:
+    def __init__(self, attack, speed):
+        self.attack = attack
+        self.speed = speed
 
